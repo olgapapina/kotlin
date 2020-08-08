f8ddb75fe3c77f95a7315321e5c93c3b6b33fa4f
Mark Punzalan <markpun@google.com>
2020-08-08 00:45:27 -0700

    RangeContainsLowering: Handle unsigned ranges.

--- compiler/testData/codegen/bytecodeText/ranges/inMixedUnsignedRange.kt
+++ compiler/testData/codegen/bytecodeText/ranges/inMixedUnsignedRange.kt
@@ -1,6 +1,3 @@
-// IGNORE_BACKEND: JVM_IR
-// TODO KT-36829 Optimize 'in' expressions in JVM_IR
-
 fun ub_ub(x: UByte, a: UByte, b: UByte) = x in a..b
 fun ub_us(x: UByte, a: UShort, b: UShort) = x in a..b
 fun ub_ui(x: UByte, a: UInt, b: UInt) = x in a..b
@@ -16,9 +13,9 @@ fun ui_us(x: UInt, a: UShort, b: UShort) = x in a..b
 fun ui_ui(x: UInt, a: UInt, b: UInt) = x in a..b
 fun ui_ul(x: UInt, a: ULong, b: ULong) = x in a..b
 
-fun ul_ub(x: ULong, a: UByte, b: UByte) = x in a..b         // ULong in range of UInt, uses non-intrinsic 'contains'
-fun ul_us(x: ULong, a: UShort, b: UShort) = x in a..b       // ULong in range of UInt, uses non-intrinsic 'contains'
-fun ul_ui(x: ULong, a: UInt, b: UInt) = x in a..b           // ULong in range of UInt, uses non-intrinsic 'contains'
+fun ul_ub(x: ULong, a: UByte, b: UByte) = x in a..b         // ULong in range of UInt, uses non-intrinsic 'contains' for non-IR backend
+fun ul_us(x: ULong, a: UShort, b: UShort) = x in a..b       // ULong in range of UInt, uses non-intrinsic 'contains' for non-IR backend
+fun ul_ui(x: ULong, a: UInt, b: UInt) = x in a..b           // ULong in range of UInt, uses non-intrinsic 'contains' for non-IR backend
 fun ul_ul(x: ULong, a: ULong, b: ULong) = x in a..b
 
 fun n_ub_ub(x: UByte, a: UByte, b: UByte) = x !in a..b
@@ -36,11 +33,12 @@ fun n_ui_us(x: UInt, a: UShort, b: UShort) = x !in a..b
 fun n_ui_ui(x: UInt, a: UInt, b: UInt) = x !in a..b
 fun n_ui_ul(x: UInt, a: ULong, b: ULong) = x !in a..b
 
-fun n_ul_ub(x: ULong, a: UByte, b: UByte) = x !in a..b      // ULong in range of UInt, uses non-intrinsic 'contains'
-fun n_ul_us(x: ULong, a: UShort, b: UShort) = x !in a..b    // ULong in range of UInt, uses non-intrinsic 'contains'
-fun n_ul_ui(x: ULong, a: UInt, b: UInt) = x !in a..b        // ULong in range of UInt, uses non-intrinsic 'contains'
+fun n_ul_ub(x: ULong, a: UByte, b: UByte) = x !in a..b      // ULong in range of UInt, uses non-intrinsic 'contains' for non-IR backend
+fun n_ul_us(x: ULong, a: UShort, b: UShort) = x !in a..b    // ULong in range of UInt, uses non-intrinsic 'contains' for non-IR backend
+fun n_ul_ui(x: ULong, a: UInt, b: UInt) = x !in a..b        // ULong in range of UInt, uses non-intrinsic 'contains' for non-IR backend
 fun n_ul_ul(x: ULong, a: ULong, b: ULong) = x !in a..b
 
+// JVM_TEMPLATES
 // 6 contains
 // 13 IFLE
 // 13 IFLT
@@ -50,3 +48,14 @@ fun n_ul_ul(x: ULong, a: ULong, b: ULong) = x !in a..b
 // 22 SIPUSH 255
 // 24 LDC 65535
 // 2 LDC 4294967295
+
+// JVM_IR_TEMPLATES
+// 0 contains
+// 64 IF(_ICMP)?GT
+// 16 IFNE
+// 80 IF
+// 0 L2I
+// 18 SIPUSH 255
+// 6 LDC 255
+// 24 LDC 65535
+// 6 LDC 4294967295
